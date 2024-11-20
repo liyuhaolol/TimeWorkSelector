9c02f395ef42d9fb16698d91dadaeb0ccbc8411a
liyuhao <liyuhaoid@sina.com>
2024-11-20 13:27:01 +0800

    增加改动

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # TimeWorkSelector
 
-参考了本站的一个框架，并进行了我自己需要的模块定制，上传jcenter纯粹为了自己这边用着方便，原作者地址：https://github.com/mrfluency/Time-Selector
+参考了本站的一个框架，并进行了我自己需要的模块定制，上传maven纯粹为了自己这边用着方便，原作者地址：https://github.com/mrfluency/Time-Selector
 
 
 ## 引用方法
 
 - 在gradle中:
 ```
-    compile 'spa.lyh.cn:time-work-selector:1.1.3'
+    compile 'spa.lyh.cn:time-work-selector:1.1.5'
 ```
 
 - 在maven中：
@@ -16,7 +16,7 @@
 <dependency>
   <groupId>spa.lyh.cn</groupId>
   <artifactId>time-work-selector</artifactId>
-  <version>1.1.3</version>
+  <version>1.1.5</version>
   <type>pom</type>
 </dependency>
 ```
---
+++ time-work-selector/build.gradle.kts
@@ -0,0 +1,112 @@
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.Properties
+import net.thebugmc.gradle.sonatypepublisher.PublishingType.AUTOMATIC
+
+plugins {
+    id("com.android.library")
+    id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.3"
+}
+
+android {
+    namespace = "spa.lyh.cn.time_work_selector"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled  = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),"proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility =  JavaVersion.VERSION_21
+    }
+}
+
+dependencies {
+    implementation("io.github.liyuhaolol:CommonUtils:1.5.7")
+}
+
+var signingKeyId = ""//签名的密钥后8位
+var signingPassword = ""//签名设置的密码
+var secretKeyRingFile = ""//生成的secring.gpg文件目录
+var ossrhUsername = ""//sonatype用户名
+var ossrhPassword = "" //sonatype密码
+
+val localProperties = project.rootProject.file("local.properties")
+
+if (localProperties.exists()) {
+    println("Found secret props file, loading props")
+    val properties = Properties()
+
+    InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
+        properties.load(reader)
+    }
+    signingKeyId = properties.getProperty("signingKeyId")
+    signingPassword = properties.getProperty("signingPassword")
+    secretKeyRingFile = properties.getProperty("secretKeyRingFile")
+    ossrhUsername = properties.getProperty("ossrhUsername")
+    ossrhPassword = properties.getProperty("ossrhPassword")
+
+} else {
+    println("No props file, loading env vars")
+}
+
+
+centralPortal {
+    username = ossrhUsername
+    password = ossrhPassword
+    name = "TimeWorkSelector"
+    group = "io.github.liyuhaolol"
+    version = "1.1.5"
+    pom {
+        //packaging = "aar"
+        name = "TimeWorkSelector"
+        description = "TimeWorkSelector"
+        url = "https://github.com/liyuhaolol/TimeWorkSelector"
+        licenses {
+            license {
+                name = "The MIT License"
+                url = "https://github.com/liyuhaolol/TimeWorkSelector/blob/master/LICENSE"
+            }
+        }
+        developers {
+            developer {
+                id = "liyuhao"
+                name = "liyuhao"
+                email = "liyuhaoid@sina.com"
+            }
+        }
+        scm {
+            connection = "scm:git@github.com/liyuhaolol/TimeWorkSelector.git"
+            developerConnection = "scm:git@github.com/liyuhaolol/TimeWorkSelector.git"
+            url = "https://github.com/liyuhaolol/TimeWorkSelector"
+        }
+
+    }
+    publishingType = AUTOMATIC
+    javadocJarTask = tasks.create<Jar>("javadocEmptyJar") {
+        archiveClassifier = "javadoc"
+    }
+
+}
+
+
+gradle.taskGraph.whenReady {
+    if (allTasks.any { it is Sign }) {
+        allprojects {
+            extra["signing.keyId"] = signingKeyId
+            extra["signing.secretKeyRingFile"] = secretKeyRingFile
+            extra["signing.password"] = signingPassword
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
+}
